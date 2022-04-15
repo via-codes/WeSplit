8e72a451ea031441dcef887a094ce82410e8fa81
Alivia Fairchild <aliviafairchild@Alivias-Air.hsd1.ut.comcast.net>
2022-04-15 10:33:19 -0600

    try this

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Alivia Fairchild on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
